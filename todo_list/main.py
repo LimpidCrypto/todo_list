382fd3afb42fdc83c28e276cb326edb001690df2
SteffenHF <120659330+SteffenHF@users.noreply.github.com>
2024-04-22 13:20:28 +0000

    refactor and utilize blueprint

--- todo_list/main.py
+++ todo_list/main.py
@@ -7,74 +7,16 @@ Requirements:
 * flask
 """
 
-import uuid
-
-from flask import Flask, request, jsonify, abort
+from flask import Flask
 from flask_cors import CORS
+from todo_list.controllers.lists import LIST_BLUEPRINT
 
-# initialize Flask server
-app = Flask(__name__)
-CORS(app)
+APP = Flask(__name__)
+CORS(APP)
 
-# create unique id for lists, entries
-todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
-todo_list_2_id = ''
-todo_list_3_id = ''
-todo_1_id = uuid.uuid4()
-todo_2_id = uuid.uuid4()
-todo_3_id = uuid.uuid4()
-todo_4_id = uuid.uuid4()
-
-# add some headers to allow cross origin access to the API on this server, necessary for using preview in Swagger Editor!
-@app.after_request
-def apply_cors_header(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
-    return response
-
-# define endpoint for getting and deleting existing todo lists
-@app.route('/list/<list_id>', methods=['GET', 'DELETE'])
-def handle_list(list_id):
-    # find todo list depending on given list id
-    list_item = None
-    for l in todo_lists:
-        if l['id'] == list_id:
-            list_item = l
-            break
-    # if the given list id is invalid, return status code 404
-    if not list_item:
-        abort(404)
-    if request.method == 'GET':
-        # find all todo entries for the todo list with the given id
-        print('Returning todo list...')
-        return jsonify([i for i in todos if i['list'] == list_id])
-    elif request.method == 'DELETE':
-        # delete list with given id
-        print('Deleting todo list...')
-        todo_lists.remove(list_item)
-        return '', 200
-
-
-# define endpoint for adding a new list
-@app.route('/list', methods=['POST'])
-def add_new_list():
-    # make JSON from POST data (even if content type is not set correctly)
-    new_list = request.get_json(force=True)
-    print('Got new list to be added: {}'.format(new_list))
-    # create id for new list, save it and return the list with id
-    new_list['id'] = uuid.uuid4()
-    todo_lists.append(new_list)
-    return jsonify(new_list), 200
-
-
-# define endpoint for getting all lists
-@app.route('/lists', methods=['GET'])
-def get_all_lists():
-    return jsonify(todo_lists)
-
+APP.register_blueprint(LIST_BLUEPRINT)
 
 if __name__ == '__main__':
     # start Flask server
-    app.debug = True
-    app.run(host='0.0.0.0', port=5000)
+    APP.debug = True
+    APP.run(host="0.0.0.0", port=3000)
