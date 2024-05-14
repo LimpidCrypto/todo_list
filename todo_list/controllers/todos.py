eb2e563f1901dac1777495e4a6df729e50572b22
SteffenHF <steffen.konermann@hamm-footwear.com>
2024-05-14 10:10:17 +0000

    complete todos api

--- todo_list/controllers/todos.py
+++ todo_list/controllers/todos.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint
 from werkzeug.exceptions import InternalServerError, NotFound, BadRequest
-from todo_list.models import create_todo, NewTodoModel, find_list_by_id
+from todo_list.models import create_todo, NewTodoModel, find_list_by_id, find_todo_list_items, find_todo_by_id, edit_todo_list_item, remove_todo_list_item
+from todo_list.controllers.validations import validate_list_existence, validate_todo_existence
 from serde import to_dict, SerdeError
 from serde.json import from_json
 from json import JSONDecodeError
@@ -12,48 +13,69 @@ TODO_BLUEPRINT = Blueprint('todo', __name__)
 @TODO_BLUEPRINT.errorhandler(InternalServerError)
 @TODO_BLUEPRINT.errorhandler(BadRequest)
 @TODO_BLUEPRINT.errorhandler(NotFound)
-def post_new_todo(list_id):
+def post_new_todo(list_id: str):
     try:
         list_uuid = UUID(list_id)
-        is_error = _validate_list_existence(list_uuid)
+        is_error = validate_list_existence(list_uuid)
         if is_error:
             return is_error
         todo_model = str(request.json).replace("'", '"')
+        new_entry = create_todo(list_uuid, from_json(NewTodoModel, todo_model))
+        return to_dict(new_entry), 201
     except (ValueError):
         return 'Invalid request', 400
-    try:
-        new_entry = create_todo(list_uuid, from_json(NewTodoModel, todo_model))
-    except (FileNotFoundError, JSONDecodeError, SerdeError) as error:
-        print(error)
-        return 'Unable to create todo', 500
-
-    return to_dict(new_entry), 201
-
-def get_todo_list(list_id):
-    try:
-        list_uuid = UUID(list_id)
-        is_error = _validate_list_existence(list_uuid)
-        if is_error:
-            return is_error
-        todo_model = str(request.json).replace("'", '"')
-    except (ValueError):
-        return 'Invalid request', 400
-    try:
-        new_entry = create_todo(list_uuid, from_json(NewTodoModel, todo_model))
-    except (FileNotFoundError, JSONDecodeError, SerdeError) as error:
-        print(error)
-        return 'Unable to create todo', 500
-
-    return to_dict(new_entry), 201
-
-def _validate_list_existence(list_id: UUID) -> Optional[Tuple[str, int]]:
-    try:
-        list_model = find_list_by_id(list_id)
-        if not list_model:
-            return 'Todo list not found', 404
-        return None
     except (FileNotFoundError, JSONDecodeError, SerdeError):
-        return 'Unable to check todo list existence', 500
+        return 'Unable to create todo', 500
 
-TODO_BLUEPRINT.add_url_rule('/list/<list_id>', view_func=get_todo_list, methods=['GET'])
+@TODO_BLUEPRINT.errorhandler(InternalServerError)
+@TODO_BLUEPRINT.errorhandler(BadRequest)
+@TODO_BLUEPRINT.errorhandler(NotFound)
+def get_todo_list_items(list_id: str):
+    try:
+        list_uuid = UUID(list_id)
+        is_error = validate_list_existence(list_uuid)
+        if is_error:
+            return is_error
+        items = find_todo_list_items(list_uuid)
+        return to_dict(items), 200
+    except (ValueError):
+        return 'Invalid UUID', 400
+    except (FileNotFoundError, JSONDecodeError, SerdeError):
+        return 'Unable to get todo list items', 500
+
+@TODO_BLUEPRINT.errorhandler(InternalServerError)
+@TODO_BLUEPRINT.errorhandler(BadRequest)
+@TODO_BLUEPRINT.errorhandler(NotFound)
+def patch_todo_list_item(list_id: str, item_id: str):
+    try:
+        list_uuid = UUID(list_id)
+        list_item_uuid = UUID(item_id)
+        is_error = validate_todo_existence(list_item_uuid)
+        if is_error:
+            return is_error
+        todo_model = str(request.json).replace("'", '"')
+        new_item = edit_todo_list_item(list_uuid, list_item_uuid, from_json(NewTodoModel, todo_model))
+        return to_dict(new_item), 200
+    except (ValueError):
+        return 'Invalid UUID', 400
+    except (FileNotFoundError, JSONDecodeError, SerdeError):
+        return 'Unable to edit todo list item', 500
+
+def delete_todo_list_item(list_id: str, item_id: str):
+    try:
+        list_uuid = UUID(list_id)
+        list_item_uuid = UUID(item_id)
+        is_error = validate_todo_existence(list_item_uuid)
+        if is_error:
+            return is_error
+        remove_todo_list_item(list_uuid, list_item_uuid)
+        return '', 204
+    except (ValueError):
+        return 'Invalid UUID', 400
+    except (FileNotFoundError, JSONDecodeError, SerdeError):
+        return 'Unable to delete todo list item', 500
+
+TODO_BLUEPRINT.add_url_rule('/list/<list_id>', view_func=get_todo_list_items, methods=['GET'])
 TODO_BLUEPRINT.add_url_rule('/list/<list_id>/item', view_func=post_new_todo, methods=['POST'])
+TODO_BLUEPRINT.add_url_rule('/list/<list_id>/item/<item_id>', view_func=patch_todo_list_item, methods=['PATCH'])
+TODO_BLUEPRINT.add_url_rule('/list/<list_id>/item/<item_id>', view_func=delete_todo_list_item, methods=['DELETE'])
